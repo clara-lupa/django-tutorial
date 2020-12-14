4771c22008c74a4ecfc11b94effed1a5dae42685
clara wolff <cwo@systemli.org>
2020-12-14 14:36:13 +0100

    setting up minimal working api following tutorial

---
+++ tutorial/quickstart/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined') # defines the data to be shown
+    serializer_class = UserSerializer # tells the computer how to (de-)serialize this model
+    permission_classes = [permissions.IsAuthenticated] # defines where the authentication is tanking place?
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
